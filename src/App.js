010e622b048ad016c6fa503b665fc0cb4a620de7
ThalKod <tmarcelin@thal.tech>
2021-04-27 09:29:54 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import Nav from "./Nav";
+
 import './App.css';
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [wrongNetworkError, setWrongNetworkError] = useState(false);
+
+  useEffect(() => {
+    initWeb3();
+    loadBlockChainData();
+
+    window.ethereum.on('accountsChanged', (accounts) => setAccount(accounts[0]));
+    window.ethereum.on('chainChanged', (_chainId) => window.location.reload());
+  }, []);
+
+  const initWeb3 = async () => {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Please Download an Ethereum compatible browser...')
+    }
+  };
+
+  const loadBlockChainData = async () => {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+    if(networkId !== 3)
+      setWrongNetworkError(true)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { wrongNetworkError && <div className="alert alert-danger" role="alert">Wrong NetWork !, Please use Ropsten tesnet !</div>}
+      <Nav account={account}/>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Nav = ({ account }) => {
+  return(
+      <nav className="navbar navbar-dark  bg-dark flex-md-nowrap p-0 shadow">
+        <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="/"
+        >
+          <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png"} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Niko Dapp
+        </a>
+
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{account}</small>
+            </small>
+          </li>
+        </ul>
+      </nav>
+  )
+};
+
+export default Nav;
